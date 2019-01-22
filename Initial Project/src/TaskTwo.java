61adc426bc198f20a1d8e109972ebd9c4bd4b29b
Thomas Sessions <Tsessions@hotmail.co.uk>
2019-01-22 09:37:36 +0000

    Initital commit with our first Java project

---
+++ Initial Project/src/TaskTwo.java
@@ -0,0 +1,17 @@
+
+public class TaskTwo {
+
+	public static void main(String[] args) {
+		
+		int firstNumber = 6;
+		int secondNumber = 7;
+		int total = Calculations.add(firstNumber, secondNumber);
+		String outputMessage = "The sum of " + 
+		firstNumber + " and " + secondNumber 
+		+ " is " + total;
+		System.out.println(outputMessage);
+	}
+	
+	
+}
+
